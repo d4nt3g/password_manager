d71015a1a9ad85b54085c07783e5f0d59bb3c905
d4nt3 <dantecostamendes@gmail.com>
2023-02-01 19:02:50 -0300

    added continue

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from functions import *
 
-print("Password Manager \n Choose an option: \n (1) Add password /// (2) See passwords")
-option = input("")
-if option in ("1", "2"):
-    if option == "1":
-        name = input("Name: ")
-        password = input("Password: ")
-        addPassword(name, password)
-    else:
-        passwordName = input("Name: ")
-        seePassword(passwordName)
+while True:
+    print("Password Manager \n Choose an option: \n (1) Add password /// (2) See passwords")
+    option = input("")
+    if option in ("1", "2"):
+        if option == "1":
+            name = input("Name: ")
+            password = input("Password: ")
+            addPassword(name, password)
+        else:
+            passwordName = input("Name: ")
+            seePassword(passwordName)
+    Continue = input("Continue? \n (y/n) ")
+    if Continue in ("y", "n"):
+        if Continue == "y":
+            print("Ok\n")
+        else:
+            input("Press enter to close the application")
+            break
